7b60f45023a95525eb56b91376f0cc71df3dc998
Pieter Noordhuis <pcnoordhuis@gmail.com>
2011-08-04 10:42:45 +0200

    Switch arguments to asserts in slowlog tests

--- tests/unit/slowlog.tcl
+++ tests/unit/slowlog.tcl
@@ -1,14 +1,14 @@
 start_server {tags {"slowlog"} overrides {slowlog-log-slower-than 1000000}} {
     test {SLOWLOG - check that it starts with an empty log} {
-        r slowlog len
-    } {0}
+        assert_equal 0 [r slowlog len]
+    }
 
     test {SLOWLOG - only logs commands taking more time than specified} {
         r config set slowlog-log-slower-than 100000
         r ping
-        assert_equal [r slowlog len] 0
+        assert_equal 0 [r slowlog len]
         r debug sleep 0.2
-        assert_equal [r slowlog len] 1
+        assert_equal 1 [r slowlog len]
     }
 
     test {SLOWLOG - max entries is correctly handled} {
@@ -17,25 +17,25 @@ start_server {tags {"slowlog"} overrides {slowlog-log-slower-than 1000000}} {
         for {set i 0} {$i < 100} {incr i} {
             r ping
         }
-        r slowlog len
-    } {10}
+        assert_equal 10 [r slowlog len]
+    }
 
     test {SLOWLOG - GET optional argument to limit output len works} {
-        llength [r slowlog get 5]
-    } {5}
+        assert_equal 5 [llength [r slowlog get 5]]
+    }
 
     test {SLOWLOG - RESET subcommand works} {
         r config set slowlog-log-slower-than 100000
         r slowlog reset
-        r slowlog len
-    } {0}
+        assert_equal 0 [r slowlog len]
+    }
 
     test {SLOWLOG - logged entry sanity check} {
         r debug sleep 0.2
         set e [lindex [r slowlog get] 0]
-        assert_equal [llength $e] 4
-        assert_equal [lindex $e 0] 105
-        assert_equal [expr {[lindex $e 2] > 100000}] 1
-        assert_equal [lindex $e 3] {debug sleep 0.2}
+        assert_equal 4 [llength $e]
+        assert_equal 105 [lindex $e 0]
+        assert_equal 1 [expr {[lindex $e 2] > 100000}]
+        assert_equal {debug sleep 0.2} [lindex $e 3]
     }
 }
